6e81bc12aa71b70dfef2e9105e4d617c989012ce
Jasper Ras <jaspert.ras@gmail.com>
2022-10-04 11:42:58 +0200

    Install perl-build also

--- plenv.plugin.zsh
+++ plenv.plugin.zsh
@@ -1,10 +1,16 @@
 GITHUB="https://github.com"
 
 [[ -z "${PLENV_HOME}" ]] && export PLENV_HOME="${HOME}/.plenv"
+[[ -z "${PLENV_PLUGINS}" ]] && export PLENV_PLUGINS="${PLENV_HOME}/plugins"
 
 _zsh_plenv_install() {
     echo "Installing plenv..."
     git clone "${GITHUB}/tokuhirom/plenv.git" "${PLENV_HOME}"
+}
+
+_zsh_plenv_install_perl_build() {
+    echo "Install perl build.."
+    git clone "${GITHUB}/tokuhirom/Perl-Build.git" "${PLENV_PLUGINS}/perl-build"
 }
 
 _zsh_plenv_load() {
@@ -14,6 +20,7 @@ _zsh_plenv_load() {
 
 # First check if installed, if not, install.
 [[ ! -d "${PLENV_HOME}" ]] && _zsh_plenv_install
+[[ ! -d "${PLENV_PLUGINS}/perl-build" ]] && _zsh_plenv_install_perl_build
 
 # Then always check again to load, otherwise it may not load upon first install.
 [[ -d "${PLENV_HOME}" ]] && _zsh_plenv_load
